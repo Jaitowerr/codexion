cc72dfd25d4ebe36b60f2ac2c1bd184866a3eace
Jaitowerr <207956068+Jaitowerr@users.noreply.github.com>
2026-06-15 18:21:30 +0200

    norminett

--- coder_executed.c
+++ coder_executed.c
@@ -6,74 +6,50 @@
 /*   By: aitorres <aitorres@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/04 00:00:00 by jaitowerr         #+#    #+#             */
-/*   Updated: 2026/06/10 16:33:55 by aitorres         ###   ########.fr       */
+/*   Updated: 2026/06/15 18:20:53 by aitorres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "coders/codexion.h"
 
-
-// void *coder_executed(void *arg)		//prueba de carlos melero para ver info sobre como coge la info los hilos
-// {
-// 	t_coder *self;
-
-//     self = (t_coder *)arg;
-// 	while (arg)
-// 	{
-// 		printf("%s\n","Holacracola");
-// 		printf("%d\n", self->id);
-// 	}
-// 	return NULL;
-// }
-
-// void *coder_executed(void *arg)	//prueba de tiempo con print usleepp
-// {
-// 	t_coder *self;
-
-// 	self = (t_coder *)arg;
-// 	printf("%ld\n", get_current_time_ms());
-// 		usleep(1100);
-// 	printf("%ld\n", get_current_time_ms());
-// 	return NULL;
-// }
-
-
-
-
+static bool	take_dongles_wrapper(t_coder *self)
+{
+	if (strcmp(self->config->scheduler, "fifo") == 0)
+	{
+		if (take_dongles_fifo(self))
+			return (true);
+	}
+	else
+	{
+		if (take_dongles_edf(self))
+			return (true);
+	}
+	return (false);
+}
 
 void	*coder_executed(void *arg)
 {
 	t_coder	*self;
 
-	self = (t_coder *)arg;	//casteo porque recibe void, no sabe que es por lo tanto casteo y debe ir bien, no se si poner un if con la funcion free
+	self = (t_coder *)arg;
 	while (self->compile_count < self->config->number_of_compiles_required
 		&& !self->ctx->someone_burned)
 	{
-		// printf("PROGRAMADOR ID-%i COMPILE COUNT: %i\n", self->id, self->compile_count);
-		self->last_compile_ms = get_current_time_ms();	//guardo el tiempo porque empiezo a esperar ahora
-		if (strcmp(self->config->scheduler, "fifo") == 0)
+		self->last_compile_ms = get_current_time_ms();
+		if (take_dongles_wrapper(self))
+			return (NULL);
+		if (compile(self))
 		{
-			if (take_dongles_fifo(self))
-				return (NULL);
-		}
-		else
-			if (take_dongles_edf(self))
-				return (NULL);
-		// if (take_dongles(self))		//coger dongle
-				// 	return (NULL);
-		if (compile(self))			// compilar
-		{
-			release_and_cooldown(self); // liberar antes de salir si se quemó durante/tras compile
+			release_and_cooldown(self);
 			return (NULL);
 		}
-		release_and_cooldown(self);	//cooldown
+		release_and_cooldown(self);
 		if (self->compile_count >= self->config->number_of_compiles_required)
-			break ; // o return (NULL); si quieres terminar el hilo directamente aquí
-		if (do_debug(self))			//debugear ver codigo el programador
+			break ;
+		if (do_debug(self))
 			return (NULL);
-		if (do_refactor(self))		//Ccorregir errores de código
+		if (do_refactor(self))
 			return (NULL);
 	}
 	return (NULL);
 }
-
--- coder_routine.c
+++ coder_routine.c
@@ -6,7 +6,7 @@
 /*   By: aitorres <aitorres@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/04 00:00:00 by jaitowerr         #+#    #+#             */
-/*   Updated: 2026/06/10 17:01:47 by aitorres         ###   ########.fr       */
+/*   Updated: 2026/06/15 18:18:41 by aitorres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -139,14 +139,6 @@ bool	take_dongles_edf(t_coder *self)
 	return (false);
 }
 
-
-
-
-
-
-
-
-
 static void	take_one_dongle_fifo(t_dongle *dongle, t_coder *self)
 {
 	t_request		req;
@@ -277,19 +269,6 @@ bool	take_dongles_fifo(t_coder *self)
 	return (false);
 }
 
-
-// static bool unlock_dongle_mutex(t_coder *self)
-// {
-// 	self->left_dongle->taken = false;
-// 	pthread_mutex_unlock(&self->left_dongle->mutex);
-// 	if (self->left_dongle != self->right_dongle)  // Solo si son distintos, caso apra 1 programador
-// 	{
-// 		self->right_dongle->taken = false;
-// 		pthread_mutex_unlock(&self->right_dongle->mutex);
-// 	}
-// 	return (true);
-// }
-
 bool check_burnout(t_coder *self)
 {
 	pthread_mutex_lock(&self->ctx->burnout_mutex);
@@ -301,171 +280,6 @@ bool check_burnout(t_coder *self)
 	pthread_mutex_unlock(&self->ctx->burnout_mutex);
 	return (false);
 }
-
-// bool check_burnout(t_coder *self)
-// {
-// 	pthread_mutex_lock(&self->ctx->burnout_mutex);
-// 	if (self->ctx->someone_burned)
-// 	{
-// 		pthread_mutex_unlock(&self->ctx->burnout_mutex);
-// 		return (true);
-// 	}
-// 	if (get_current_time_ms() - self->last_compile_ms > self->config->time_to_burnout)
-// 	{
-// 		self->ctx->someone_burned = true;
-// 		log_status(self, "burned out");
-// 		// printf("***************BURNOUT programador ID-%i\n", self->id);
-// 		pthread_mutex_unlock(&self->ctx->burnout_mutex);
-// 		return (true);
-// 	}
-// 	pthread_mutex_unlock(&self->ctx->burnout_mutex);
-// 	return (false);
-// }
-
-// ... includes arriba ...
-
-// bool	take_dongles_fifo(t_coder *self)
-// {
-// 	t_dongle	*first;
-// 	t_dongle	*second;
-
-// 	// Esperar a que ambos dongles estén disponibles
-// 	while (get_current_time_ms() < self->left_dongle->available_at_ms
-// 		|| get_current_time_ms() < self->right_dongle->available_at_ms)
-// 	{
-// 		if (check_burnout(self))
-// 			return (true);
-// 		usleep(100);
-// 	}
-	
-// 	if (check_burnout(self))
-// 		return (true);
-
-// 	// Caso especial: 1 solo programador
-// 	if (self->left_dongle == self->right_dongle)
-// 	{
-// 		pthread_mutex_lock(&self->left_dongle->mutex);
-// 		self->left_dongle->taken = true;
-// 		log_status(self, "has taken a dongle");
-// 		if (check_burnout(self))
-// 		{
-// 			self->left_dongle->taken = false;
-// 			pthread_mutex_unlock(&self->left_dongle->mutex);
-// 			return (true);
-// 		}
-// 		while (!check_burnout(self))
-// 			usleep(1000);
-// 		return (true);
-// 	}
-
-// 	// Orden pares/impares para evitar deadlock
-// 	if (self->id % 2 == 0)
-// 	{
-// 		first = self->right_dongle;
-// 		second = self->left_dongle;
-// 	}
-// 	else
-// 	{
-// 		first = self->left_dongle;
-// 		second = self->right_dongle;
-// 	}
-
-// 	// Coger el primer dongle
-// 	pthread_mutex_lock(&first->mutex);
-// 	first->taken = true;
-// 	log_status(self, "has taken a dongle");
-	
-// 	// Verificar burnout después del primer dongle
-// 	if (check_burnout(self))
-// 	{
-// 		first->taken = false;
-// 		pthread_mutex_unlock(&first->mutex);
-// 		return (true);
-// 	}
-
-// 	// Coger el segundo dongle
-// 	pthread_mutex_lock(&second->mutex);
-// 	second->taken = true;
-// 	log_status(self, "has taken a dongle");
-
-// 	// Verificar burnout después del segundo dongle
-// 	if (check_burnout(self))
-// 	{
-// 		// Liberar ambos dongles si se quema
-// 		first->taken = false;
-// 		pthread_mutex_unlock(&first->mutex);
-// 		second->taken = false;
-// 		pthread_mutex_unlock(&second->mutex);
-// 		return (true);
-// 	}
-	
-// 	return (false);
-// }
-
-// // Por ahora EDF usa exactamente la misma lógica que FIFO; luego la cambiaremos
-// bool	take_dongles_edf(t_coder *self)
-// {
-// 	return (take_dongles_fifo(self));
-// }
-
-// bool	take_dongles(t_coder *self)
-// {
-// 	t_dongle	*first;
-// 	t_dongle	*second;
-
-// 	while (get_current_time_ms() < self->left_dongle->available_at_ms
-// 		|| get_current_time_ms() < self->right_dongle->available_at_ms)
-// 	{
-// 		if (check_burnout(self))
-// 			return (true);
-// 		usleep(100);
-// 	}
-// 	if (check_burnout(self))
-// 		return (true);
-
-// 	// Caso especial: 1 coder (mismo dongle para ambos lados)
-// 	if (self->left_dongle == self->right_dongle)
-// 	{
-// 		pthread_mutex_lock(&self->left_dongle->mutex);
-// 		log_status(self, "has taken a dongle");
-// 		self->left_dongle->taken = false;
-// 		pthread_mutex_unlock(&self->left_dongle->mutex);
-// 		while (!check_burnout(self))
-// 			usleep(1000);
-// 		return (true);
-// 	}
-
-// 	// Orden pares e impares para romper el deadlock circular
-// 	if (self->id % 2 == 0)
-// 	{
-// 		first = self->right_dongle;
-// 		second = self->left_dongle;
-// 	}
-// 	else
-// 	{
-// 		first = self->left_dongle;
-// 		second = self->right_dongle;
-// 	}
-
-// 	pthread_mutex_lock(&first->mutex);
-// 	first->taken = true;
-// 	log_status(self, "has taken a dongle");
-
-// 	pthread_mutex_lock(&second->mutex);
-// 	second->taken = true;
-// 	log_status(self, "has taken a dongle");
-
-// 	if (check_burnout(self))
-// 	{
-// 		first->taken = false;
-// 		pthread_mutex_unlock(&first->mutex);
-// 		second->taken = false;
-// 		pthread_mutex_unlock(&second->mutex);
-// 		return (true);
-// 	}
-// 	return (false);
-// }
-
 
 bool	compile(t_coder *self)
 {
@@ -483,19 +297,3 @@ bool	compile(t_coder *self)
 	
 	return (check_burnout(self));
 }
-
-// bool	compile(t_coder *self)
-// {
-// 	//COMPILAR
-// 	// printf("   - COMPILAR programador ID-%i\n", self->id);
-// 	usleep(self->config->time_to_compile * 1000);
-// 	log_status(self, "is compiling");
-
-// 	if (check_burnout(self))
-// 	{
-// 		// Soltar dongles porque los tenemos cogidos
-// 		return (unlock_dongle_mutex(self));
-// 	}
-// 	return (false);
-// }
-
